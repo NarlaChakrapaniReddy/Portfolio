b4ed660b71e341ac5473a26d80602dd3f7dd7b9d
NarlaChakrapaniReddy <nchakrapanireddy123@gmail.com>
2025-06-27 21:41:15 +0530

    adding some styles

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 var typed=new Typed(".text",{
-strings:["Front-end Developer"," Traveller","Web Developer"],
+strings:["Front-end Developer","Java Developer","Cloud Engineer"],
 typeSpeed:100,
 backSpeed:100,
 backDelay:800,
